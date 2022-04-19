495a54399efd9380f3a2328647bef6a7fb5b8d25
Carlos Javier Loya Amaya <carlos.loya@Abitat.com.mx>
2022-04-18 19:59:49 -0600

    Agregando objeto con propiedades semana 2 Node JS

--- example2.js
+++ example2.js
@@ -13,3 +13,18 @@ const myObject = {
 console.log(myObject)
 console.log(myObject.name)
 console.log(myObject["name"])
+
+const ajolonauta = {
+    name: "Woopa",
+    mission: "Node JS",
+    sayHelloToExplorers: function () {
+        console.log("Soy el ajolonauta")
+    },
+    tellMeMore: function () {
+        console.log(`Ajolonauta: ${this.name}`)
+    },
+}
+
+console.log("Ajolonauta")
+ajolonauta.sayHelloToExplorers()
+ajolonauta.tellMeMore()
